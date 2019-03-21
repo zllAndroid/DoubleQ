cb17ec5ab5a45a7ff252f85e633db10c0e70127c
zllAndroid <970705279@qq.com>
2019-03-21 13:58:52 +0800

    032104Byzll

--- app/src/main/java/com/doubleq/xm6leefunz/about_base/BaseActivity.java
+++ app/src/main/java/com/doubleq/xm6leefunz/about_base/BaseActivity.java
@@ -89,7 +89,6 @@ public class BaseActivity extends BaseActivityForResult  implements BGASwipeBack
     public void onSwipeBackLayoutExecuted() {
         mSwipeBackHelper.swipeBackward();
         AppManager.getAppManager().finishActivity(this);
-//       overridePendingTransition(cn.bingoogolapple.swipebacklayout.R.anim.bga_sbl_activity_swipeback_enter, cn.bingoogolapple.swipebacklayout.R.anim.bga_sbl_activity_swipeback_exit);
         overridePendingTransition(0,0);
 
     }
@@ -102,7 +101,6 @@ public class BaseActivity extends BaseActivityForResult  implements BGASwipeBack
         }
         mSwipeBackHelper.backward();
         AppManager.getAppManager().finishActivity(this);
-//        overridePendingTransition(cn.bingoogolapple.swipebacklayout.R.anim.bga_sbl_activity_swipeback_enter, cn.bingoogolapple.swipebacklayout.R.anim.bga_sbl_activity_swipeback_exit);
 
         overridePendingTransition(0,0);
     }
